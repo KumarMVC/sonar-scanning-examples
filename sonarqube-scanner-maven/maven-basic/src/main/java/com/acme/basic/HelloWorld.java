06bf8199dbcb5bd19919df05357b3fb6d90cd868
KumarMVC <31248575+KumarMVC@users.noreply.github.com>
2019-10-29 09:09:27 +0100

    Update HelloWorld.java

--- sonarqube-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
+++ sonarqube-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
@@ -3,11 +3,11 @@ package com.acme.basic;
 public class HelloWorld {
 
   void sayHello() {
-    System.out.println("Hello World!");
+    logger.log("Hello World!");
   }
 
   void notCovered() {
-    System.out.println("This method is not covered by unit tests");
+    logger.log("This method is not covered by unit tests");
   }
 
 }
